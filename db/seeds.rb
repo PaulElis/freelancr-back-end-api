20f5189af350447aeb12166b79a728a3bbe43d98
github username <github email address>
2018-08-05 19:50:26 -0400

    My site ready for deployment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.create(username: 'jar_jar_binks', password: '1234', first_name: 'Jar Jar', 
 User.create(username: 'yoda', password: '1234', first_name: 'Yo', last_name: 'da')
 User.create(username: 'jabba_the_hutt', password: '1234', first_name: 'Jabba', last_name: 'The Hutt')
 
-Contract.create(title: 'Design new front end',
+Contract.create(title: 'Redesign Front End',
                 summary: 'You, the client, are hiring me, Paul Elis, to develop a web application as outlined in our previous correspondence. The agreed payment is at the end of the document.',
                 details: 'I will provide suggestions for designs or templates for the look-and-feel, layout and functionality of your web site. I will provide any design support necessary to complete the design of the site, fully develop the site, and implement a custom back end so that you can continue to maintain the site on your own in the future.',
                 milestones: "• Pattern Lab setup / Project setup
@@ -35,10 +35,19 @@ possibilities of such damages.",
 
 Contract.create(title: 'Integrate AUTH',
                 summary: 'You, the client, are hiring me to develop a web application as outlined in our previous correspondence. The agreed payment plan is at the end of the document.',
-                details: "",
-                milestones: "",
-                legal: "",
-                copyright: "",
+                details: "I will provide suggestions for designs or templates for the look-and-feel, layout and functionality of your web site. I will provide any design support necessary to complete the design of the site, fully develop the site, and implement a custom back end so that you can continue to maintain the site on your own in the future.",
+                milestones: "• Pattern Lab setup / Project setup
+                • Layout all elements in the stylesheet.pdf (these are the micro-level building blocks to build up from)
+                • Layout all macro-elements needed for 25% of the Vis/*.jpg files (I'm not sure which would be given highest priority)
+                • Feedback on milestone
+                • Browser testing of milestone",
+                legal: "I will take the utmost care and attention to ensure that my code is error-free and adequately
+future-proofed, but due to the rapidly-evolving nature of web standards, browsers and
+programming languages it is not possible to guarantee that code will function as intended indefinitely and so I can't be liable to you or any third party for damages, including lost profits, lost
+savings or other incidental, consequential or special damages arising out of the operation of or
+inability to operate this web site and any other web pages, even if you have advised me of the
+possibilities of such damages.",
+                copyright: "You guarantee to me that any elements of text, graphics, photos, designs, trademarks, or other artwork that you provide us for inclusion in the web site are either owned by your good self, or that you have permission to use them.",
                 compensation: '10000',
                 developer_id: 9,
                 contractor_id: 1,
@@ -49,10 +58,19 @@ Contract.create(title: 'Integrate AUTH',
 
 Contract.create(title: 'Redesign UX design',
                 summary: 'You, the client, are hiring me to develop a web application as outlined in our previous correspondence. The agreed payment plan is at the end of the document.',
-                details: "",
-                milestones: "",
-                legal: "",
-                copyright: "",
+                details: "I will provide suggestions for designs or templates for the look-and-feel, layout and functionality of your web site. I will provide any design support necessary to complete the design of the site, fully develop the site, and implement a custom back end so that you can continue to maintain the site on your own in the future.",
+                milestones: "• Pattern Lab setup / Project setup
+                • Layout all elements in the stylesheet.pdf (these are the micro-level building blocks to build up from)
+                • Layout all macro-elements needed for 25% of the Vis/*.jpg files (I'm not sure which would be given highest priority)
+                • Feedback on milestone
+                • Browser testing of milestone",
+                legal: "I will take the utmost care and attention to ensure that my code is error-free and adequately
+future-proofed, but due to the rapidly-evolving nature of web standards, browsers and
+programming languages it is not possible to guarantee that code will function as intended indefinitely and so I can't be liable to you or any third party for damages, including lost profits, lost
+savings or other incidental, consequential or special damages arising out of the operation of or
+inability to operate this web site and any other web pages, even if you have advised me of the
+possibilities of such damages.",
+                copyright: "You guarantee to me that any elements of text, graphics, photos, designs, trademarks, or other artwork that you provide us for inclusion in the web site are either owned by your good self, or that you have permission to use them.",
                 compensation: '3000',
                 developer_id: 2,
                 contractor_id: 4,
@@ -63,10 +81,19 @@ Contract.create(title: 'Redesign UX design',
 
 Contract.create(title: 'Build out additional features on platform',
                 summary: 'You, the client, are hiring me to develop a web application as outlined in our previous correspondence. The agreed payment plan is at the end of the document.',
-                details: "",
-                milestones: "",
-                legal: "",
-                copyright: "",
+                details: "I will provide suggestions for designs or templates for the look-and-feel, layout and functionality of your web site. I will provide any design support necessary to complete the design of the site, fully develop the site, and implement a custom back end so that you can continue to maintain the site on your own in the future.",
+                milestones: "• Pattern Lab setup / Project setup
+                • Layout all elements in the stylesheet.pdf (these are the micro-level building blocks to build up from)
+                • Layout all macro-elements needed for 25% of the Vis/*.jpg files (I'm not sure which would be given highest priority)
+                • Feedback on milestone
+                • Browser testing of milestone",
+                legal: "I will take the utmost care and attention to ensure that my code is error-free and adequately
+future-proofed, but due to the rapidly-evolving nature of web standards, browsers and
+programming languages it is not possible to guarantee that code will function as intended indefinitely and so I can't be liable to you or any third party for damages, including lost profits, lost
+savings or other incidental, consequential or special damages arising out of the operation of or
+inability to operate this web site and any other web pages, even if you have advised me of the
+possibilities of such damages.",
+                copyright: "You guarantee to me that any elements of text, graphics, photos, designs, trademarks, or other artwork that you provide us for inclusion in the web site are either owned by your good self, or that you have permission to use them.",
                 compensation: '5000',
                 developer_id: 6,
                 contractor_id: 3,
@@ -77,10 +104,19 @@ Contract.create(title: 'Build out additional features on platform',
 
 Contract.create(title: 'Build a functioning backend',
                 summary: 'You, the client, are hiring me to develop a web application as outlined in our previous correspondence. The agreed payment plan is at the end of the document.',
-                details: "",
-                milestones: "",
-                legal: "",
-                copyright: "",
+                details: "I will provide suggestions for designs or templates for the look-and-feel, layout and functionality of your web site. I will provide any design support necessary to complete the design of the site, fully develop the site, and implement a custom back end so that you can continue to maintain the site on your own in the future.",
+                milestones: "• Pattern Lab setup / Project setup
+                • Layout all elements in the stylesheet.pdf (these are the micro-level building blocks to build up from)
+                • Layout all macro-elements needed for 25% of the Vis/*.jpg files (I'm not sure which would be given highest priority)
+                • Feedback on milestone
+                • Browser testing of milestone",
+                legal: "I will take the utmost care and attention to ensure that my code is error-free and adequately
+future-proofed, but due to the rapidly-evolving nature of web standards, browsers and
+programming languages it is not possible to guarantee that code will function as intended indefinitely and so I can't be liable to you or any third party for damages, including lost profits, lost
+savings or other incidental, consequential or special damages arising out of the operation of or
+inability to operate this web site and any other web pages, even if you have advised me of the
+possibilities of such damages.",
+                copyright: "You guarantee to me that any elements of text, graphics, photos, designs, trademarks, or other artwork that you provide us for inclusion in the web site are either owned by your good self, or that you have permission to use them.",
                 compensation: '8000',
                 developer_id: 7,
                 contractor_id: 8,
@@ -91,10 +127,19 @@ Contract.create(title: 'Build a functioning backend',
 
 Contract.create(title: 'Build a functioning backend',
                 summary: 'You, the client, are hiring me to develop a web application as outlined in our previous correspondence. The agreed payment plan is at the end of the document.',
-                details: "",
-                milestones: "",
-                legal: "",
-                copyright: "",
+                details: "I will provide suggestions for designs or templates for the look-and-feel, layout and functionality of your web site. I will provide any design support necessary to complete the design of the site, fully develop the site, and implement a custom back end so that you can continue to maintain the site on your own in the future.",
+                milestones: "• Pattern Lab setup / Project setup
+                • Layout all elements in the stylesheet.pdf (these are the micro-level building blocks to build up from)
+                • Layout all macro-elements needed for 25% of the Vis/*.jpg files (I'm not sure which would be given highest priority)
+                • Feedback on milestone
+                • Browser testing of milestone",
+                legal: "I will take the utmost care and attention to ensure that my code is error-free and adequately
+future-proofed, but due to the rapidly-evolving nature of web standards, browsers and
+programming languages it is not possible to guarantee that code will function as intended indefinitely and so I can't be liable to you or any third party for damages, including lost profits, lost
+savings or other incidental, consequential or special damages arising out of the operation of or
+inability to operate this web site and any other web pages, even if you have advised me of the
+possibilities of such damages.",
+                copyright: "You guarantee to me that any elements of text, graphics, photos, designs, trademarks, or other artwork that you provide us for inclusion in the web site are either owned by your good self, or that you have permission to use them.",
                 compensation: '4000',
                 developer_id: 10,
                 contractor_id: 11,
